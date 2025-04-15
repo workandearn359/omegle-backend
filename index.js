236f1db31324427b0c348126b45f62c6ca843e3d
forgameplay359 <forgameplay359@gmail.com>
2025-04-15 14:24:08 +0530

    fix: use dynamic port for Render deploy

--- index.js
+++ index.js
@@ -1,80 +1,13 @@
 const express = require("express");
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+// Yeh line fix hai:
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
 });
 
-let waitingUser = null;
-
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  if (waitingUser) {
-    const partner = waitingUser;
-    waitingUser = null;
-
-    socket.partner = partner;
-    partner.partner = socket;
-
-    socket.emit("partner-found");
-    partner.emit("partner-found");
-  } else {
-    waitingUser = socket;
-  }
-
-  socket.on("signal", (data) => {
-    if (socket.partner) {
-      socket.partner.emit("signal", data);
-    }
-  });
-
-  socket.on("message", (msg) => {
-    if (socket.partner) {
-      socket.partner.emit("message", msg);
-    }
-  });
-
-  socket.on("next", () => {
-    if (socket.partner) {
-      socket.partner.partner = null;
-      socket.partner.emit("partner-left");
-    }
-    socket.partner = null;
-    if (waitingUser && waitingUser !== socket) {
-      const partner = waitingUser;
-      waitingUser = null;
-
-      socket.partner = partner;
-      partner.partner = socket;
-
-      socket.emit("partner-found");
-      partner.emit("partner-found");
-    } else {
-      waitingUser = socket;
-    }
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-    if (waitingUser === socket) {
-      waitingUser = null;
-    }
-    if (socket.partner) {
-      socket.partner.partner = null;
-      socket.partner.emit("partner-left");
-    }
-  });
-});
-
-server.listen(10000, () => {
-  console.log("Server listening on port 10000");
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
