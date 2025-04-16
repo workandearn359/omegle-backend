c8d49e21beeca7f8c7c0dbfac83bcbeb8055a7cf
forgameplay359 <forgameplay359@gmail.com>
2025-04-16 13:01:56 +0530

    Fix matching logic to avoid self/reconnect issues

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get('/healthz', (req, res) => {
   res.send('OK');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
